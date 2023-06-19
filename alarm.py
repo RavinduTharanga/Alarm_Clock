e59b0b199f2dc610de291ac4315f088d868ed85b
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2023-06-18 23:06:04 -0400

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+# from playsound import playsound
+
+alarmtime = input("Enter the time HH:MM \n")
+alarmhour = alarmtime[0:2]
+alarmminute= alarmtime[3:5]
+print("Setting up alarm..")
+
+while True:
+    now = datetime.now()
+    current_hour=now.strftime("%H")
+    current_minute = now.strftime("%M")
+    # current_seconds = now.strftime("%S")
+    # current_period = now.strftime("%p")
+    if alarmhour==current_hour and alarmminute==current_minute:
+                    print("Wake Up!")
+                    # playsound('audio.mp3')
+                    break
